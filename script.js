dc8f5961cdedde85de0bc3533dd2f038d307ab58
andresalexanderss <andresalexanderss@ufps.edu.co>
2023-12-13 21:16:15 -0500

    Se coloca la interface en ingles

--- script.js
+++ script.js
@@ -36,11 +36,11 @@ function search(pat, txt, q) {
 
   if (posiciones.length > 0) {
     const resultado = document.getElementById("resultado");
-    resultado.value = `La palabra "${pat}" se encuentra ${
+    resultado.value = `The "${pat}" is matched ${
       posiciones.length
-    } veces en las posiciones: ${posiciones.join(", ")}`;
+    } number of tmes on the file: ${posiciones.join(", ")}`;
   } else {
-    resultado.value = `La palabra "${pat}" no se encuentra en el texto.`;
+    resultado.value = `The  "${pat}" It is not found in the text.`;
   }
 
   return posiciones;
@@ -59,7 +59,7 @@ function cargarTXT() {
       const contenido = e.target.result;
       texto = contenido;
       const resultado = document.getElementById("resultado");
-      resultado.value = "Archivo cargado correctamente.";
+      resultado.value = "File uploaded successfully.";
     };
     reader.readAsText(archivo);
   }
@@ -70,7 +70,7 @@ let posiciones = [];
 function buscar() {
   const patron = document.getElementById("patron").value;
   if (texto == "") {
-    alert("Debe cargar un archivo de texto primero.");
+    alert("You must upload a text file first.", "Error");
   }
   let q = 101;
   posiciones = search(patron, texto, q);
@@ -85,36 +85,42 @@ function generarPDF() {
 
   template.innerHTML = htmlConResaltado;
 
-  html2pdf().set(
-    {
+  html2pdf()
+    .set({
       margin: 1,
       filename: "resultado.pdf",
       html2canvas: { scale: 3 },
       jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-    }
-  ).from(htmlConResaltado).save().catch(err => console.log(err));
+    })
+    .from(htmlConResaltado)
+    .save()
+    .catch((err) => console.log(err));
+  const resultado = document.getElementById("resultado");
+  resultado.value = "PDF generated successfully.";
 }
 
 function resaltarPalabra(texto, patron) {
-  const lineas = texto.split('\n'); // Dividir el texto por saltos de línea
-  let resultado = '';
+  const lineas = texto.split("\n");
+  let resultado = "";
 
   lineas.forEach((linea, indice) => {
-    const partes = linea.split(new RegExp(`(${patron})`, 'gi'));
+    const partes = linea.split(new RegExp(`(${patron})`, "gi"));
 
     partes.forEach((part, index) => {
       if (index < partes.length - 1) {
-        resultado += part.toLowerCase() === patron.toLowerCase()
-          ? `<span style="color: red;">${part}</span>`
-          : part;
+        resultado +=
+          part.toLowerCase() === patron.toLowerCase()
+            ? `<span style="color: red;">${part}</span>`
+            : part;
       } else {
-        resultado += part.toLowerCase() === patron.toLowerCase()
-          ? `<span style="color: red;">${part}</span>`
-          : part;
+        resultado +=
+          part.toLowerCase() === patron.toLowerCase()
+            ? `<span style="color: red;">${part}</span>`
+            : part;
       }
     });
 
-    resultado += '<br>'; // Agregar <br> después de cada línea
+    resultado += "<br>";
   });
 
   return resultado;
